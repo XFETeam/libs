8983b63e1d35af011102d9f38e3c02efd7178167
PsAiL <allenps1991@hotmail.com>
2020-05-06 10:59:20 +0000

    release: 修复部署路径

--- example/config/paths.js
+++ example/config/paths.js
@@ -42,7 +42,7 @@ function getServedPath(appPackageJson) {
 // config after eject: we're in ./config/
 module.exports = {
   dotenv: resolveApp('.env'),
-  appBuild: resolveApp(`build/${appName}`),
+  appBuild: resolveApp(`build`),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveApp('src/index.js'),
