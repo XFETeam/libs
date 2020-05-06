7553943de2fcfa288ddc491e40a5ab6f7722dc8c
PsAiL <allenps1991@hotmail.com>
2020-05-06 10:31:42 +0000

    release: 0.0.3

--- example/config/webpack.config.prod.js
+++ example/config/webpack.config.prod.js
@@ -129,9 +129,6 @@ module.exports = {
         include: paths.appSrc
       },
       {
-        // "oneOf" will traverse all following loaders until one will
-        // match the requirements. When no loader matches it will fall
-        // back to the "file" loader at the end of the loader list.
         oneOf: [
           // "url" loader works just like "file" loader but it also embeds
           // assets smaller than specified size as data URLs to avoid requests.
