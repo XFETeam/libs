a53c5e2672e1ec1ca6548eeed1dcce7642aef757
HUANGZHE <huangzhe@kingsoft.com>
2020-05-06 18:02:41 +0800

    init

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
-# libs
+## 用于剑网三推栏App金山通行证授权
 
-> library 仓库，一个分支作为一个 library 存在。
+### 使用方法
+```javascript
+// 按顺序先引入 daily-app-auth脚本，然后引入get-daily-account-info.js脚本
 
-## 使用流程
-
-1. 通过 template 分支 checkout 并进行如组件等工具开发
-2. 完成 checkout 后阅读 [TRAVIS.md](./TRAVIS.md) 文件，这里会讲述目前持续集成的基本部署流程
-3. 开始进行开发，完成开发后阅读 `CHANGELOG_FORMAT.md`
-4. 使用 webstorm 等工具全局替换如 `<%= projectName %>` 等字段
-5. 最终决定通过 npm 上线时， 更新 `package.json` 中 `version` 版本， 并以 `release:` 作为 `commit message` 进行提交 
-
-## TRAVIS CI 地址
-
-https://travis-ci.com/XFETeam/libs
-
-## 作者
-She Ailun
+// 可以在window.THIRD_PARTY_AUTH获取相关授权信息
+console.log(window.THIRD_PARTY_AUTH);
+```
---
+++ daily-app-auth.js
@@ -0,0 +1,98 @@
+(function() {
+  /**
+   * 获取 UA
+   * @return {string}
+   */
+  function getUserAgent() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    return userAgent.toLowerCase();
+  }
+
+  /**
+   * 是否是 daily app
+   * @return {boolean}
+   */
+  function isDailyApp() {
+    return /jianghudaily/i.test(getUserAgent());
+  }
+
+  if (!isDailyApp()) {
+    return;
+  }
+
+  var hostname = window.location.hostname;
+  var isMasterEnv = /xoyo.com$/.test(hostname) && hostname.indexOf('test') === -1;
+  var xoyoUrl = isMasterEnv ? 'ws.xoyo.com' : 'test-ws.xoyo.com';
+
+  /**
+   * 获取参数名
+   * @param name
+   * @param url
+   * @returns {string|null}
+   */
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[[]]/g, '$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  }
+
+  function removeURLParameter(url, parameter) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts = url.split('?');
+    if (urlparts.length >= 2) {
+      var prefix = encodeURIComponent(parameter) + '=';
+      var pars = urlparts[1].split(/[&;]/g);
+
+      //reverse iteration as may be destructive
+      for (var i = pars.length; i-- > 0; ) {
+        //idiom for string.startsWith
+        if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+          pars.splice(i, 1);
+        }
+      }
+
+      return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+    }
+    return url;
+  }
+
+  /**
+   * 在不刷新页面的情况下替换 url
+   * @param {string} nextUrl - 下一个将要替换的 url, 注意必须同源
+   */
+  function replaceUrlWithoutReloading(nextUrl) {
+    window.history.replaceState({}, document.title, nextUrl);
+  }
+
+  var token = getParameterByName('token') || getParameterByName('ssoToken');
+
+  if (!token) {
+    return;
+  }
+
+  var nextSearch = removeURLParameter(window.location.search, 'token');
+  replaceUrlWithoutReloading(window.location.pathname + removeURLParameter(nextSearch, 'ssoToken') + window.location.hash);
+
+  window.__DAILY_APP_LOGIN_BY_TOKEN__ = function(response) {
+    window.__DAILY_APP_LOGIN_BY_TOKEN__ = undefined;
+    if (response.status !== 1 /* 失败 */) {
+      window.location.href = 'https://jx3.seasungame.com/error-pages/index.html?login-expired';
+      throw new Error('DAILY_APP_AUTH 登录态失效，请重新登录: ' + JSON.stringify(response));
+    }
+  };
+
+  document.writeln(
+    '<script src="//' +
+      xoyoUrl +
+      '/core/jhdaily/login_by_token?ts=' +
+      new Date().getTime() +
+      '&callback=__DAILY_APP_LOGIN_BY_TOKEN__&limit_guest=1&token=' +
+      token +
+      '"><' +
+      '/script>'
+  );
+})();
